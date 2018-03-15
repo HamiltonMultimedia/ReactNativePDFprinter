efdefcac0ece772139c389995b6d354a03ecb17e
Hamilton Multimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2018-03-15 08:33:12 -0400

    First Commit

---
+++ android/app/src/main/java/com/pdfform/MainApplication.java
@@ -0,0 +1,48 @@
+package com.pdfform;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.facebook.react.ReactApplication;
+import cl.json.RNSharePackage;
+import com.imagepicker.ImagePickerPackage;
+import com.parkerdan.htmltopdf.RNHTMLtoPDFPackage;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    protected boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new RNSharePackage(),
+            new ImagePickerPackage(),
+            new RNHTMLtoPDFPackage()
+      );
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
---
+++ index.ios.js
@@ -0,0 +1,35 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import Demo from './Demo';
+
+export default class PdfForm extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Demo />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
+
+AppRegistry.registerComponent('PdfForm', () => PdfForm);
